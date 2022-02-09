01559ffd12a98d20d73a75c788f8c072f71e42d2
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-02-09 13:05:05 +0530

    cars

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-// let menu = document.querySelector("#menu-btn");
-// let navbar = document.querySelector(".navbar");
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
 
-// menu.onclick = () => {
-//   menu.classList.toggle("fa-times");
-//   navbar.classList.toString("active");
-// };
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toString("active");
+};
+var swiper = new Swiper(".vehicles-slider", {
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
