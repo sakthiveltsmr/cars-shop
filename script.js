df31a1210cbc45679cc56615c7ca9985df398092
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-02-11 14:08:07 +0530

    car

--- script.js
+++ script.js
@@ -3,32 +3,27 @@ let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
-  navbar.classList.toString("active");
+  navbar.classList.toggle("active");
 };
 
 document.querySelector("#login-btn").onclick = () => {
   document.querySelector(".login-form-container").classList.toggle("active");
 };
+
 document.querySelector("#close-login-form").onclick = () => {
   document.querySelector(".login-form-container").classList.remove("active");
 };
+
 window.onscroll = () => {
-  if (window.scrollY > 0) {
-    document.querySelector(".header").classList.add("active");
-  } else {
-    document.querySelector(".header").classList.remove("active");
-  }
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
-};
-window.onload = () => {
+
   if (window.scrollY > 0) {
     document.querySelector(".header").classList.add("active");
   } else {
     document.querySelector(".header").classList.remove("active");
   }
 };
-
 var swiper = new Swiper(".vehicles-slider", {
   grabCursor: true,
   centeredSlides: true,
